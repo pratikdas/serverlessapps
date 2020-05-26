61bcf5b4b022068864d208088d767c31ccd0f5d3
Das, Pratik (Cognizant) <pratik.das@cognizant.com>
2020-05-26 12:39:47 +0400

    aaded basic sam app

---
+++ inventory-svc-app/InventorySvcAppFunction/src/test/java/inventorysvcapp/AppTest.java
@@ -0,0 +1,25 @@
+package inventorysvcapp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import inventorysvcapp.App;
+import inventorysvcapp.GatewayResponse;
+
+public class AppTest {
+  @Test
+  public void successfulResponse() {
+    App app = new App();
+    GatewayResponse result = (GatewayResponse) app.handleRequest(null, null);
+    assertEquals(result.getStatusCode(), 200);
+    assertEquals(result.getHeaders().get("Content-Type"), "application/json");
+    String content = result.getBody();
+    assertNotNull(content);
+    System.out.println(content+ " "+content.contains("\"inventory data\""));
+    assertTrue(content.contains("\"message\""));
+    assertTrue(content.contains("\"inventory data\""));
+    assertTrue(content.contains("\"location\""));
+  }
+}
